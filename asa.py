01a0e658da6ceb091ad96e948d1bc2a01186e708
gbercsenyi <gabor.bercsenyi@snt.hu>
2020-04-19 21:27:29 +0200

    Refactoring GET and POST methods, create 2 private method to handle GET and POST and evaluate the REST response.

--- asa.py
+++ asa.py
@@ -24,27 +24,51 @@ class ASA:
         response = requests.request("POST", url, headers=headers, data=payload, verify=False)
         self.token = response.headers['X-Auth-Token']
 
+    def __get_method(self, url):
+        url = url
+        response = requests.request("GET", url, headers=self.headers, verify=False)
+        if 400 <= response.status_code <= 499:
+            return f"Failed {json.dumps(response.json())}"
+        if 200 <= response.status_code <= 299:
+            for item in response.json()["items"]:
+                print(item["name"])
+
+    def __post_method(self, url, payload):
+        url = url
+        payload = payload
+        if payload != {}:
+            response = requests.request("POST", url, headers=self.headers, data=json.dumps(payload), verify=False)
+        else:
+            response = requests.request("POST", url, headers=self.headers, data=payload, verify=False)
+        if 400 <= response.status_code <= 499:
+            return f"Failed {json.dumps(response.json())}"
+        if 200 <= response.status_code <= 299:
+            return "Success"
+
     def get_time_range_objects(self):
+        print("Getting the Time-Range Objects:")
         url = f"https://{self.host}/api/objects/timeranges"
-        payload = {}
-        response = requests.request("GET", url, headers=self.headers, data=payload, verify=False)
-        print(json.dumps(response.json(), indent=2))
+        print(self.__get_method(url))
 
     def post_time_range_object(self, time_range_name, start_time, end_time):
+        print("Posting new object:", time_range_name)
         url = f"https://{self.host}/api/objects/timeranges"
-        payload = {"kind": "object#TimeRange",
-                   "name": time_range_name,
-                   "value": {
-                       "start": str(start_time),
-                       "end": str(end_time),
-                       "periodic": []
-                       }
-                   }
-        response = requests.request("POST", url, headers=self.headers, data=json.dumps(payload), verify=False)
-        if response.status_code == 400:
-            print(response.json())
-        if response.status_code == 201:
-            print("Success")
+        payload = {
+            "kind": "object#TimeRange",
+            "name": time_range_name,
+            "value": {
+                "start": str(start_time),
+                "end": str(end_time),
+                "periodic": []
+                }
+            }
+        print(self.__post_method(url, payload))
+
+    def save_config(self):
+        print("saving the config")
+        url = f"https://{self.host}/api/commands/writemem"
+        payload = {}
+        print(self.__post_method(url, payload))
 
 
 def encode_username_password(username, password):
@@ -60,7 +84,8 @@ def main(*args, **kwargs):
     asa5506 = ASA(args[0]['host'], authorization)
     print("Token:", asa5506.token)
     asa5506.get_time_range_objects()
-    asa5506.post_time_range_object("teszt3", kwargs['start'], kwargs['end'])
+    asa5506.post_time_range_object("teszt4", kwargs['start'], kwargs['end'])
+    asa5506.save_config()
 
 
 if __name__ == '__main__':
@@ -71,4 +96,3 @@ if __name__ == '__main__':
     start = f"{start.hour}:{start.minute} {start.strftime('%B')} {start.day} {start.year}"
     end = f"{end.hour}:{end.minute} {end.strftime('%B')} {end.day} {end.year}"
     main(myasa, start=start, end=end)
-
